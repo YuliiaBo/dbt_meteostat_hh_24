8a817a68880f2e071a047447d747bfb0d888ff70
YuliiaBo <121019328+YuliiaBo@users.noreply.github.com>
2025-01-24 15:11:26 +0100

    prep_w

--- models/prep/prep_weather_daily.sql
+++ models/prep/prep_weather_daily.sql
@@ -10,4 +10,18 @@ add_features AS (
         , DATE_PART('week', date) AS cw             -- number of the week of year
         , TO_CHAR(date, 'FMmonth') AS month_name    -- name of the month
         , TO_CHAR(date, 'FMday') AS weekday         -- name of the weekday
-    FROM dai
+    FROM daily_data 
+),
+add_more_features AS (
+    SELECT *
+        , (CASE 
+            WHEN month_name in ('december','january','february') THEN 'winter'
+            WHEN month_name in ('march','april','may') THEN 'spring'
+            WHEN month_name in ('june','july','august') THEN 'summer'
+            WHEN month_name in ('september','october','november') THEN 'autumn'
+        END) AS season
+    FROM add_features
+)
+SELECT *
+FROM add_more_features
+ORDER BY date
